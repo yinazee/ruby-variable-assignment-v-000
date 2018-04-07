c0d3c91bbd3859bafea167ae9e7459179cc1ada7
Yina Zhu <yinagit@gmail.com>
2018-04-07 20:24:55 +0000

    automatically backed up by learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,5 +1,5 @@
 # Code your solution in variable.rb
-describe "./variable.rb" do
+describe "variable" do
 
   it "defined a local variable called greeting and set it equal to 'So far so good.'" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
